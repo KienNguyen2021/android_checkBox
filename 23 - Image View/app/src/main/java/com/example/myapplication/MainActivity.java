10c882e9524726358810171cb00a0c04acfa6640
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-08-08 11:10:46 -0600

    first commit

--- 23 - Image View/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 23 - Image View/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgHinh;
+
+    RelativeLayout manhinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
         imgHinh = (ImageView) findViewById(R.id.imageViewHinh);
         imgHinh.setImageResource(R.drawable.android_image2);
+
+        manhinh =(RelativeLayout) findViewById(R.id.manHinh);
+
+        manhinh.setBackgroundResource(R.drawable.ruongBacThang);
+
     }
 }
